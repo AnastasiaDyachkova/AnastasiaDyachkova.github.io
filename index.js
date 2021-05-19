ef48c0ee023afdc9da92667eff7ec52ac4b9bac6
AnastasiaDyachkova <79082425+AnastasiaDyachkova@users.noreply.github.com>
2021-05-19 23:24:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 function main () {
     const panelsElements = document.querySelector('.wrapper');
     
-  }
-  
-  window.onload = main;
-  
+
+    const redCounter = document.querySelector('.red');
+    const greenCounter = document.querySelector('.green');
+    const blueCounter = document.querySelector('.blue');
+
+  let redCount = 0;
+  let greenCount = 0;
+  let blueCount = 0;
+
+    redCounter.addEventListener('click', (e)=>{
+      redCount++;
+      document.querySelector('.red').innerHTML = redCount;
+      document.querySelector('.red-counter').innerHTML = redCount;
+    });
+
+    greenCounter.addEventListener('click', (e)=>{
+      greenCount++;
+      document.querySelector('.green').innerHTML = greenCount;
+      document.querySelector('.green-counter').innerHTML = greenCount;
+    });
+
+    blueCounter.addEventListener('click', (e)=>{
+      blueCount++;
+      document.querySelector('.blue').innerHTML = blueCount;
+      document.querySelector('.blue-counter').innerHTML = blueCount;
+    });
+    
+}
+
+window.onload = main;
